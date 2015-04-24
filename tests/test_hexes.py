c7722012c35f2bb1ef888d486f57db16988194b2
Kit La Touche <kit@simpleenergy.com>
2015-04-24 11:41:19 -0600

    Setting the cursor to invisible breaks CircleCI
    But is not really important, so we can skip it if it causes problems.
    
    Also, let's assert that we have a TTY.

--- tests/test_hexes.py
+++ tests/test_hexes.py
@@ -3,12 +3,15 @@ import pytest
 from hexes import Application
 
 
-@pytest.mark.xfail(reason="Curses again.", run=False)
 def test_setup_and_teardown():
     with pytest.raises(RuntimeError):
         with Application():
             raise RuntimeError
 
 
-def test_placeholder():
-    assert True
+def test_is_a_tty():
+    import os
+    import sys
+    assert os.isatty(sys.stdout.fileno())
+    assert os.isatty(sys.stderr.fileno())
+    assert os.isatty(sys.stdin.fileno())
